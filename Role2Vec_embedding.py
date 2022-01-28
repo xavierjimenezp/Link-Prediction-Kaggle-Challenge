597dd0c118eef8f431dd2af9c6e02a9297f49a44
Xavier <xavierjimenezp@gmail.com>
2022-01-28 18:15:59 +0100

    changed matrix filling

--- Role2Vec_embedding.py
+++ Role2Vec_embedding.py
@@ -6,23 +6,15 @@ from Node2Vec_embedding import return_embeddings
 
 start_time = time.time()
 
-# Create a graph
-G = nx.read_edgelist('data/edgelist.txt', delimiter=',', create_using=nx.Graph(), nodetype=int)
-nodes = list(G.nodes())
-n = G.number_of_nodes()
-m = G.number_of_edges()
-print('Number of nodes:', n)
-print('Number of edges:', m)
-
-
 #--------------------------------------------------------------------------------------#
 #----------------------------# MODEL PARAMETERS TO CHANGE #----------------------------#
-parameters = {'walk_number': 10, 'walk_length': 80, 'dimensions': 128, 'window_size': 5, 
+parameters = {'walk_number': 20, 'walk_length': 80, 'dimensions': 128, 'window_size': 5, 
               'epochs': 1, 'workers': 7}
 #--------------------------------------------------------------------------------------#
+print('Learning embedding with parameters')
+print(parameters)
 
-
-embeddings = return_embeddings(G, Role2Vec, parameters)
+embeddings = return_embeddings(Role2Vec, parameters)
 np.save('data/embedding_diff2vec_wn{:d}_wl{:d}_d{:d}_ws{:d}'.format(parameters['walk_number'], parameters['walk_length'],
                                                                parameters['dimensions'], parameters['window_size']), 
         embeddings)
