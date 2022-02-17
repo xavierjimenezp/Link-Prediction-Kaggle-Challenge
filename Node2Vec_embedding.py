ed4a68103ad772f6ce7f62f13f92507730538b8a
xavierjimenezp <xavierjimenezp@gmail.com>
2022-02-17 20:13:14 +0100

    Top2

--- Node2Vec_embedding.py
+++ Node2Vec_embedding.py
@@ -25,28 +25,17 @@ def return_embeddings(G, model, parameters):
 
 #--------------------------------------------------------------------------------------#
 #----------------------------# MODEL PARAMETERS TO CHANGE #----------------------------#
-parameters = {'walk_number': 10, 'walk_length': 15, 'dimensions': 64, 'window_size': 5, 'workers': 5}
+parameters = {'walk_number': 10, 'walk_length': 15, 'dimensions': 64, 'window_size': 5, 'workers': 3}
 #--------------------------------------------------------------------------------------#
 print('Learning embedding with parameters')
 print(parameters)
 #--------------------------------------------------------------------------------------#
-G = nx.read_weighted_edgelist('data/unique_co_authors_graph.txt', delimiter=',', create_using=nx.Graph(), nodetype=int)
+G = nx.read_weighted_edgelist('data/unique_citation_graph_train.txt', delimiter=',', create_using=nx.Graph(), nodetype=int)
 print(list(G.edges(data=True))[0])
-#--------------------------------------------------------------------------------------#
-numeric_indices = [index for index in range(G.number_of_nodes())]
-node_indices = sorted([node for node in G.nodes()])
-if not numeric_indices == node_indices:
-    max_nodes = max(G.nodes())
-    missing_list = list(set([index for index in range(max_nodes)]) - set(node_indices))
-    print("The node indexing is wrong. But the graph was complete by node not connected\n")
-    for i in tqdm(missing_list, desc='Waiting for completion'):
-        G.add_edge(i,i,weight = 0)
-if max_nodes != 137861:
-    print("Error")
 #--------------------------------------------------------------------------------------#
 
 embeddings = return_embeddings(G, Node2Vec, parameters)
-np.save('data/embedding_n2v_unique_co_authors_wn{:d}_wl{:d}_d{:d}_ws{:d}'.format(parameters['walk_number'], parameters['walk_length'],
+np.save('data/embedding_n2v_unique_citation_graph_train_wn{:d}_wl{:d}_d{:d}_ws{:d}'.format(parameters['walk_number'], parameters['walk_length'],
                                                                parameters['dimensions'], parameters['window_size']), 
         embeddings)
 
